63695a3a1fbd7bf44f1e7596fbe9da2a0cd16083
Remo de Oliveira Gresta <remoogg@gmail.com>
2021-07-10 23:37:14 -0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,104 @@
+const { request, response } = require('express');
 const express = require('express');
 const { v4:uuidv4 } = require('uuid')
 const app = express();
 app.use(express.json());
 
-const costumers = [];
+const customers = [];
+
+function isThereASccount(request,response,next){
+
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+    if(!customer){
+        return response.status(400).json({message:"error: customer not found"});
+    }
+
+    request.customer = customer;
+    return next();
+}
+
+function getBalance(statement){
+
+    const balance = statement.reduce((acc,operation)=>{
+        if(operation.type === 'credit'){
+            return acc + operation.amount;
+        }else{
+            return acc - operation.amount;
+        }
+    },0)
+    return balance;
+
+}
 
 app.post("/account",(request,response)=>{
 
     const { cpf, name } = request.body;
-    const id = uuidv4();
+   
+    const checkCustomer = customers.some(
+        (customer)=> customer.cpf === cpf     
+    );
+    
 
-    costumers.push({cpf,name,id,statement:[]})
+    if(checkCustomer){
+        return response.status(400).json({error:"Customer alredy exists"})
+    }
 
-    response.status(201).send("cadastro feito")
+    customers.push({
+        cpf,
+        name,
+        id:uuidv4(),
+        statement:[]})
+
+    return response.status(201).send("Register check")
    
 });
+// app.use(isThereASccount);
+app.get("/statement",isThereASccount,(request,response)=>{
+    ///    /statement/:cpf
+    // const { cpf } = request.params;
+    const { customer } = request;
+    return response.json(customer.statement);
+    
 
+});
+
+app.post("/deposit",isThereASccount,(request,response)=>{
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    // console.log(amount)
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type:"credit"
+    }
+    customer.statement.push(statementOperation);
+    return response.status(200).send()
+});
+
+app.post("/withdraw",isThereASccount,(request,response)=>{
+
+    const { amount } = request.body;
+
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+    if(balance < amount){
+        return response.status(400).json({"message":"Insufficient founds"})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type:"debit"     
+    }
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send()
+});
 
 app.listen(3333)
